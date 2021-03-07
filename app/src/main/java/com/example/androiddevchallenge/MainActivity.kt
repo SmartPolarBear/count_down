b66f06735ebebb7600e51af0b7c3e258c445272e
SmartPolarBear <CleverCoolBear@outlook.com>
2021-03-07 22:51:47 +0800

    finished the UI

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -17,45 +17,200 @@ package com.example.androiddevchallenge
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import com.example.androiddevchallenge.ui.theme.CountDownTheme
+import androidx.compose.runtime.livedata.observeAsState
 
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CountDownTheme {
-                Main()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "Count Down") },
+                            elevation = 16.dp,
+                        )
+                    },
+                    floatingActionButton = {
+                        StartEndFloatingActionButton(start = false, onClick = { /*TODO*/ })
+                    }
+                )
+                {
+                    Surface(color = MaterialTheme.colors.background) {
+                        Main(mainViewModel = mainViewModel)
+                    }
+                }
             }
         }
     }
 }
 
-// Start building your app here!
 @Composable
-fun Main() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+fun NumericUpDown(
+    value: Int = 0,
+    range: IntRange = IntRange(0, 60),
+    onValueChange: (Int) -> Unit
+) {
+
+    val updateValue = { newValue: Int ->
+        onValueChange(
+            when {
+                range.contains(newValue) -> newValue
+                newValue < range.first -> range.first
+                newValue > range.last -> range.last
+                else -> 0
+            }
+        )
+    }
+
+    val updateValueWithText = { str: String ->
+        if (str.isNotEmpty() && str.isDigitsOnly()) {
+            updateValue(str.toInt())
+        } else {
+            updateValue(0)
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(start = 8.dp, end = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        OutlinedButton(onClick = { updateValue(value + 1) }) {
+            Icon(
+                imageVector = Icons.Default.ArrowUpward,
+                contentDescription = stringResource(R.string.up)
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(bottom = 4.dp)
+        ) {
+            OutlinedTextField(
+                value = value.toString(),
+                onValueChange = { updateValueWithText(it) },
+                singleLine = true,
+                modifier = Modifier
+                    .requiredWidth(52.dp)
+            )
+        }
+
+        OutlinedButton(onClick = { updateValue(value - 1) }) {
+            Icon(
+                imageVector = Icons.Default.ArrowDownward,
+                contentDescription = stringResource(R.string.down)
+            )
+        }
     }
 }
+
+@Composable
+fun TimeUnitText(
+    text: String
+) {
+    Text(
+        text = text,
+    )
+}
+
+// Start building your app here!
+@Composable
+fun Main(mainViewModel: MainViewModel) {
+    Column() {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+            val hours: Int by mainViewModel.hours.observeAsState(0)
+            NumericUpDown(value = hours, range = 0..23,
+                onValueChange = { mainViewModel.onHoursChange(it) })
+            TimeUnitText(text = "H")
+
+            val minutes: Int by mainViewModel.minutes.observeAsState(0)
+            NumericUpDown(value = minutes, range = 0..60,
+                onValueChange = { mainViewModel.onMinutesChange(it) })
+            TimeUnitText(text = "M")
+
+            val seconds: Int by mainViewModel.seconds.observeAsState(0)
+            NumericUpDown(value = seconds, range = 0..60,
+                onValueChange = { mainViewModel.onSecondsChange(it) })
+            TimeUnitText(text = "S")
+        }
+    }
+
+
+}
+
+
+@Composable
+fun StartEndFloatingActionButton(
+    start: Boolean,
+    onClick: () -> Unit
+) {
+    FloatingActionButton(onClick = onClick) {
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Icon(
+                imageVector = when {
+                    start -> Icons.Default.PlayArrow
+                    else -> Icons.Default.Pause
+                },
+                contentDescription = null
+            )
+            Text(
+                modifier = Modifier
+                    .padding(start = 8.dp, top = 3.dp),
+                text = when {
+                    start -> stringResource(id = R.string.start)
+                    else -> stringResource(id = R.string.end)
+                }
+            )
+        }
+    }
+}
+
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
+    val mainViewModel = MainViewModel()
     CountDownTheme {
-        Main()
+        Main(mainViewModel)
     }
 }
 
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
+    val mainViewModel = MainViewModel()
     CountDownTheme(darkTheme = true) {
-        Main()
+        Main(mainViewModel)
     }
 }
--- app/src/main/java/com/example/androiddevchallenge/MainViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainViewModel.kt
@@ -1,2 +1,30 @@
 package com.example.androiddevchallenge
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val _hours = MutableLiveData(0)
+    val hours: LiveData<Int> = _hours
+
+    private val _minutes = MutableLiveData(0)
+    val minutes: LiveData<Int> = _minutes
+
+    private val _seconds = MutableLiveData(0)
+    val seconds: LiveData<Int> = _seconds
+
+    fun onHoursChange(newHours: Int) {
+        _hours.value = newHours
+    }
+
+    fun onMinutesChange(newMinutes: Int) {
+        _minutes.value = newMinutes
+    }
+
+    fun onSecondsChange(newSeconds: Int) {
+        _seconds.value = newSeconds
+    }
+
+}
